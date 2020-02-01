a6937c34414381d422b617be65183b4ccd498ddf
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-01-31 16:36:12 -0800

    first commit

---
+++ js-vue-basics/s1v3/app.js
@@ -0,0 +1,16 @@
+const helloWorld = new Vue({
+    el: '#helloVue',
+    data: {
+        message: "This is my first template!",
+        title: "Hello, world!"
+    }
+})
+
+const example = new Vue({
+    el: '#example',
+    data: {
+        message: "This is for cats.",
+        title: "Hello",
+        name: "Gato"
+    }
+})
